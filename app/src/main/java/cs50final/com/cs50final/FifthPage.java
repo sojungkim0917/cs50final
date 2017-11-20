5750dcb067c3d66f3bb4c22cac28d259bd807f1d
SoJung Kim <sojungkim0917@gmail.com>
2017-11-20 13:08:22 -0500

    New update with extra pages!

--- app/src/main/java/cs50final/com/cs50final/FifthPage.java
+++ app/src/main/java/cs50final/com/cs50final/FifthPage.java
@@ -1,7 +1,7 @@
 package cs50final.com.cs50final;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;package cs50final.com.cs50final;
+import android.support.v7.app.AppCompatActivity;
 
 
 public class FifthPage extends AppCompatActivity {

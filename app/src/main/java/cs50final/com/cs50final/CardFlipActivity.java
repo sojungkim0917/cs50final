e8e561796f375cbeb50bb7d0ee071be5e1be80da
SoJung Kim <sojungkim0917@gmail.com>
2017-11-19 22:11:08 -0500

    1 page added test

--- app/src/main/java/cs50final/com/cs50final/CardFlipActivity.java
+++ app/src/main/java/cs50final/com/cs50final/CardFlipActivity.java
@@ -183,3 +183,4 @@ public class CardFlipActivity extends Activity
         }
     }
 }
+17
---
+++ app/src/main/java/cs50final/com/cs50final/FourthPage.java
@@ -0,0 +1,8 @@
+package cs50final.com.cs50final;
+
+/**
+ * Created by sishiryeety on 11/19/17.
+ */
+
+public class FourthPage {
+}

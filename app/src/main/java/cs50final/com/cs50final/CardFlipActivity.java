5bc865eeb9a88cb4760fd65d25ad4ce7f2fa0110
caw1230 <31663787+caw1230@users.noreply.github.com>
2017-11-27 08:21:41 -0500

    Updated with ABC main and homepage images

--- app/src/main/java/cs50final/com/cs50final/CardFlipActivity.java
+++ app/src/main/java/cs50final/com/cs50final/CardFlipActivity.java
@@ -30,6 +30,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import static android.view.MenuItem.*;
+
 /**
  * Demonstrates a "card-flip" animation using custom fragment transactions ({@link
  * android.app.FragmentTransaction#setCustomAnimations(int, int)}).

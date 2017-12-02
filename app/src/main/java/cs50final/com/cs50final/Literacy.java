0545e98c21d5f4eaac8caa87e240706ee9efa7da
q0w9e8r7t6y5 <syeety@me.com>
2017-12-02 14:47:46 -0500

    -

--- app/src/main/java/cs50final/com/cs50final/Literacy.java
+++ app/src/main/java/cs50final/com/cs50final/Literacy.java
@@ -1,24 +1,130 @@
-/*
- Feel free to turn this into multiple choice
- */
-
 package cs50final.com.cs50final;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
-import android.support.v4.app.NavUtils;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-public class Literacy extends Homepage {
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+/**
+ * Created by sishiryeety on 12/2/17.
+ */
+
+public class Literacy extends AppCompatActivity {
+
+    private TextView mQuestion;
+
+    private Button mButtonChoice1, mButtonChoice2, mButtonChoice3, mButtonChoice4;
+
+    private int mScore = 0;
+    private int mQuestionNumber = 0;
+    private String mAnswer;
+
+    private Firebase mQuestionRef, mchoice1Ref, mchoice2Ref, mchoice3Ref, mchoice4Ref, mAnswerRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.literacy);
+        super.OnCreate(SavedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mQuestion = (TextView) findViewById(R.id.question);
+
+        mButtonChoice1 = (Button) findViewById(R.id.answer1);
+        mButtonChoice2 = (Button) findViewById(R.id.answer2);
+        mButtonChoice3 = (Button) findViewById(R.id.answer3);
+        mButtonChoice4 = (Button) findViewById(R.id.answer4);
+
+        updateQuestion();
+
+    }
+
+    public void updateQuestion() {
+        mQuestionRef = new Firebase("https://cs50final-7bf22.firebaseio.com/"+ mQuestionNumber +"/question");
+        mQuestionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String question = dataSnapshot.getValue(String.class);
+                mQuestion.setText(question);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        mchoice1Ref= new Firebase("https://cs50final-7bf22.firebaseio.com/"+ mQuestionNumber +"/choice1");
+        mchoice1Ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String choice = dataSnapshot.getValue(String.class);
+                mButtonChoice1.setText(choice);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        mchoice2Ref= new Firebase("https://cs50final-7bf22.firebaseio.com/"+ mQuestionNumber +"/choice2");
+        mchoice2Ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String choice = dataSnapshot.getValue(String.class);
+                mButtonChoice2.setText(choice);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        mchoice3Ref= new Firebase("https://cs50final-7bf22.firebaseio.com/"+ mQuestionNumber +"/choice3");
+        mchoice3Ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String choice = dataSnapshot.getValue(String.class);
+                mButtonChoice3.setText(choice);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        mchoice4Ref= new Firebase("https://cs50final-7bf22.firebaseio.com/"+ mQuestionNumber +"/choice4");
+        mchoice4Ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String choice = dataSnapshot.getValue(String.class);
+                mButtonChoice1.setText(choice);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        mAnswerRef = new Firebase("https://cs50final-7bf22.firebaseio.com/"+ mQuestionNumber +"/answer");
+        mAnswerRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                mAnswer = dataSnapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        mQuestionNumber++;
+
     }
 }
